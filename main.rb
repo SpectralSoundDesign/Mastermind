a22d627c0f087b02a40debc27da5a24701398d95
Will Richards <spectralsounddesign@gmail.com>
2022-10-01 23:29:11 -0400

    added intro, player_choice, and check methods

--- main.rb
+++ main.rb
@@ -1,31 +1,87 @@
 class Game
+  @@gameover = false
   def initialize(secret_code)
     @round = 1
     @secret_code = secret_code
   end
 
   def intro
-    puts "---------------------"
-    puts "WELCOME TO MASTERMIND"
-    puts "---------------------"
+    puts "------------------------------------------"
+    puts "          WELCOME TO MASTERMIND           "
+    puts "------------------------------------------"
     puts "New game? Y/N"
-    yes_no = gets
-    #fix later
-    if yes_no == "y" || yes_no == "Y" || yes_no == "n" || yes_no == "N"
-      puts "test"
+    yes_no = gets.to_s.strip.downcase
+    
+    if yes_no == 'y'
+      play(@secret_code)
+    elsif yes_no == 'n'
+      exit
+    else
+      intro()
     end
   end
 
-  def play(yes_no)
-    puts "test"
+  def hide_code(code)
+    code.map { |i| "X" }
   end
 
-  def display
-    puts @secret_code
+  def play(code)
+    puts "=========================================="
+    puts "code:"
+    puts "   #{hide_code(code)}"
+    puts "=========================================="
+
+    while @@gameover != true
+      puts "Round #{@round}:"
+      choice = player_choice()
+      puts "=========================================="
+      check(code, choice)
+      puts "=========================================="
+    end
   end
 
   def player_choice
+    @round += 1
+    arr_len = 4
+    player_guess = Array.new
+
+    arr_len.times do |i|
+      puts "Choice #{i + 1}:"
+      choice = gets.strip
+      player_guess.push(choice)
+    end
+
+    puts "#{player_guess}"
+    player_guess
   end
+
+  def check(code, choice)
+    score = Array.new
+
+    code.each_with_index do |v, i|
+      if choice[i] == v
+        score.push("correct")
+      elsif code.include?(choice[i]) == true
+        score.push("right color wrong spot")
+      else
+        score.push("incorrect")
+      end
+    end
+
+    score.each do |v|
+      if v == "correct"
+        @@gameover == true
+      else
+        @@gameover == false
+        break
+      end
+    end
+
+    puts "#{@@gameover}"
+    puts "#{code}"
+    puts "#{score}"
+  end
+
 end
 
 class CodeGen
